d05ee2b2201c9f937e440e44edd35f88a8016c74
Romain Pierucci <pierucci.romain@gmail.com>
2023-02-05 18:02:32 +0100

    add: curry with logs

--- exercises/curry/correction/curry.js
+++ exercises/curry/correction/curry.js
@@ -7,3 +7,16 @@ export const curry =
       return (...moreArgs) => curry(fn)(...args, ...moreArgs);
     }
   };
+
+// Version avec console.log pour tester :
+/* export const curry = (fn) => {
+  console.log("fn length", fn.length);
+  return (...args) => {
+    console.log("args", args);
+    if (args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return (...moreArgs) => curry(fn)(...args, ...moreArgs);
+    }
+  };
+}; */
